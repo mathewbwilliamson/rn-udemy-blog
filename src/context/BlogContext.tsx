bf9da2ee6ea2e53e008ffa99a0136aa6cde21c94
Matt Williamson <mathewbwilliamson@gmail.com>
2021-01-18 12:08:25 -0500

    Added data into context

--- src/context/BlogContext.tsx
+++ src/context/BlogContext.tsx
@@ -2,6 +2,20 @@ import React from 'react';
 
 export const BlogContext = React.createContext({});
 
-export const BlogProvider: React.FC<any> = ({ children }) => {
-    return <BlogContext.Provider>{children}</BlogContext.Provider>;
+export interface Post {
+    title: string;
+    id: number;
+}
+
+export const BlogProvider: React.FC<Post> = ({ children }) => {
+    const blogPosts: Post[] = [
+        { title: 'Blog #1', id: 1 },
+        { title: 'Blog #2', id: 2 },
+        { title: 'Blog #3', id: 3 },
+    ];
+    return (
+        <BlogContext.Provider value={blogPosts}>
+            {children}
+        </BlogContext.Provider>
+    );
 };
--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 import { NavigationStackScreenComponent } from 'react-navigation-stack';
+import { BlogContext, Post } from '../context/BlogContext';
 
 export const IndexScreen: NavigationStackScreenComponent = () => {
+    const blogPosts = useContext(BlogContext) as Post[];
+
     return (
         <View>
             <Text>Index Screen</Text>
+            <FlatList
+                data={blogPosts}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={(renderItem) => {
+                    const blogPost = renderItem.item;
+                    return <Text>{blogPost.title}</Text>;
+                }}
+            />
         </View>
     );
 };
